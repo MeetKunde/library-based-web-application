3d3afd95929404899ee4aeb6213adccbe4e377d5
Karol Janic <karoljanic97@gmail.com>
2023-08-06 14:21:57 +0200

    Repaired intersection bugs

--- frontend/src/app/dashboard/board/Board.ts
+++ frontend/src/app/dashboard/board/Board.ts
@@ -28,11 +28,17 @@ export class Board {
     static initialize(boardId: string, bounds: [number, number, number, number], showAxis: boolean) {
         Board.board = JXG.JSXGraph.initBoard(boardId, {boundingbox: bounds, axis: showAxis, keepAspectRatio: true});
         Board.board.on('down', Board.handleBoardClick);
+        Board.board.on('update', Board.correctIntersectionPoints);
+
         Board.action = ActionEnum.NONE;
         Board.shapeClicked = false;
 
         Board.capitalLettersCounter = 0;
         Board.lowercaseLettersCounter = 0;
+
+        Board.shapesAccumlator = [];
+
+        //Board.correctIntersectionPoints();
 
         /*
         const p1 = Board.board.create('point', [0, 0]);
@@ -103,6 +109,7 @@ export class Board {
         });
 
         point.on('down', (event: any) => { event.stopPropagation(); Board.handleShapeClick(event, point); });
+        Board.board.update();
 
         return point;
     }
@@ -119,6 +126,7 @@ export class Board {
         });
 
         point.on('down', (event: any) => { Board.handleShapeClick(event, point); });
+        Board.board.update();
 
         return point;
     }
@@ -183,6 +191,7 @@ export class Board {
 
     private static handleBoardClick(event: any): void {
         if(!Board.shapeClicked) {
+            event.stopPropagation();
             Board.handleClick(event, (x: number, y: number) => {
                 return Board.createPoint(x, y);
             });
@@ -191,18 +200,20 @@ export class Board {
     }
 
     private static handleShapeClick(event: any, shape: any): void {
-        Board.shapeClicked = true;
-        event.stopPropagation();
+        if(!Board.shapeClicked) {
+            Board.shapeClicked = true;
+            event.stopPropagation();
 
-        if(shape.elementClass === JXG.OBJECT_CLASS_POINT) {
-            Board.handleClick(event, (x: number, y: number) => {
-                return shape;
-            });
-        }
-        else {
-            Board.handleClick(event, (x: number, y: number) => {
-                return Board.createGliderPoint(x, y, shape);
-            });
+            if(shape.elementClass === JXG.OBJECT_CLASS_POINT) {
+                Board.handleClick(event, (x: number, y: number) => {
+                    return shape;
+                });
+            }
+            else {
+                Board.handleClick(event, (x: number, y: number) => {
+                    return Board.createGliderPoint(x, y, shape);
+                });
+            }
         }
     }
 
@@ -250,6 +261,20 @@ export class Board {
         }
     }
 
+    private static pointExist(usrCoords: [s: number, x: number, y: number]): [canCreate: boolean, duplication: any] {
+        var exist = false;
+        var duplication = undefined;
+        for(let el in Board.board.objects) {
+            if(JXG.isPoint(Board.board.objects[el]) && Board.board.objects[el].hasPoint(usrCoords[1], usrCoords[2])) {
+                exist = true;
+                duplication = Board.board.objects[el];
+                break;
+            }
+        }
+
+        return [exist, duplication];
+    }
+
     private static getCoords(event: any): [x: number, y: number, canCreate: boolean, duplication: any] {
         var fingerIndex = undefined;
         if(event[JXG.touchProperty]) {
@@ -259,17 +284,9 @@ export class Board {
         var pos = Board.board.getMousePosition(event, fingerIndex);
         var coords = new JXG.Coords(JXG.COORDS_BY_SCREEN, pos, Board.board);
 
-        var canCreate = true;
-        var duplication = undefined;
-        for(let el in Board.board.objects) {
-            if(JXG.isPoint(Board.board.objects[el]) && Board.board.objects[el].hasPoint(coords.scrCoords[1], coords.scrCoords[2])) {
-                canCreate = false;
-                duplication = Board.board.objects[el];
-                break;
-            }
-        }
+        var searchResult = Board.pointExist(coords.usrCoords);
 
-        return [coords.usrCoords[1], coords.usrCoords[2], canCreate, duplication];
+        return [coords.usrCoords[1], coords.usrCoords[2], !searchResult[0], searchResult[1]];
     }
 
     private static solveCollisions(shape: any): void {
@@ -283,16 +300,59 @@ export class Board {
             if(el.elType == 'segment' || el.elType == 'line' || el.elType == 'circle') {
                 const point1 = JXG.Math.Geometry.meet(shape.stdform, el.stdform, 0, Board.board);
                 const point2 = JXG.Math.Geometry.meet(shape.stdform, el.stdform, 1, Board.board);
-                
+
                 const intersection1 = Board.createPoint(point1.usrCoords[1], point1.usrCoords[2]);
                 intersection1.makeIntersection(shape, el, 0, 0);
 
-
                 if(el.elType == 'circle' || shape.elType == 'circle') {
                     const intersection2 =  Board.createPoint(point2.usrCoords[1], point2.usrCoords[2]);  
-                    intersection2.makeIntersection(shape, el, 1, 1);  
-                }                
+                    intersection2.makeIntersection(shape, el, 1, 1); 
+                }               
             }
         }
+
+        Board.board.update();
+    }
+
+    private static correctIntersectionPoints(): any {
+        console.log('correcting...');
+
+        var intersectionPoints: any[] = [];
+        var nonIntersectionPoints: any[] = [];
+
+        for(let el in Board.board.objects) {
+            const point = Board.board.objects[el];
+
+            if((point.elType == 'point' || point.elType == 'glider') && point.getAttribute('visible')) {
+                nonIntersectionPoints.push(point);
+            }
+
+            if(point.elType == 'intersection') {
+                intersectionPoints.push(point);
+            }
+        }
+
+        for(const ip of intersectionPoints) {
+            var newOpacity = 1;
+            for(const nip of nonIntersectionPoints) {
+                const dist = Board.distance(ip, nip);
+                //console.log(dist)
+                
+                if(dist < 1 || Number.isNaN(dist)) {
+                    newOpacity = 0;
+                    break;
+                }
+            }
+
+            ip.setAttribute({ opacity: newOpacity });
+            ip.label.setAttribute({ opacity: newOpacity });
+        }
+    }
+
+    private static distance(point1: any, point2: any): number {
+        const dx = point1.coords.usrCoords[1] - point2.coords.usrCoords[1];
+        const dy = point1.coords.usrCoords[2] - point2.coords.usrCoords[2];
+
+        return Math.sqrt(dx * dx + dy * dy);
     }
 };
